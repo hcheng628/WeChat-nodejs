ca2ef3b40ff1f8dacc852cf7cae90e72a5f85a7b
superhcheng <chenghongyu628@gmail.com>
2017-03-28 22:26:12 -0400

    add socket.io

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const fs = require('fs');
 const socketIO = require('socket.io');
+const http = require('http');
 
 const PUBLIC_PATH = path.join(__dirname, '../public/');
 
@@ -11,19 +12,35 @@ const nodeApp_PORT = process.env.PORT || 3000;
 // console.log(__dirname + '/../public');
 console.log(PUBLIC_PATH);
 
+
 var nodeApp = express();
-
-nodeApp.get(wechat_HOMEPAGE,(request, response) => {
-  fs.readFile(PUBLIC_PATH + 'index.html', 'utf-8', (err, data)=>{
-    // console.log(PUBLIC_PATH + 'index.html');
-    // console.log(data);
-    response.send(data);
-  });
-});
-
-nodeApp.listen(nodeApp_PORT, ()=>{
+var nodeServer = nodeApp.listen(nodeApp_PORT, ()=> {
   console.log(`Node Application Up n' Running @ ${nodeApp_PORT} ......`);
 });
+var io = socketIO.listen(nodeServer);
+
+io.on('connection',(new_socket)=>{
+  console.log('Server: New User Connected');
+  console.log("Server Socket: ", new_socket);
+});
+
+io.on('disconnect',(new_socket)=>{
+  console.log('Server: New User got Disconnected');
+  console.log("Server Socket: ", new_socket);
+});
+
+nodeApp.use(express.static(PUBLIC_PATH));
+// nodeApp.get(wechat_HOMEPAGE,(request, response) => {
+//   fs.readFile(PUBLIC_PATH + 'index.html', 'utf-8', (err, data)=>{
+//     // console.log(PUBLIC_PATH + 'index.html');
+//     // console.log(data);
+//     response.send(data);
+//   });
+// });
+
+// nodeApp.listen(nodeApp_PORT, ()=>{
+//   console.log(`Node Application Up n' Running @ ${nodeApp_PORT} ......`);
+// });
 
 module.exports = {
   nodeApp
